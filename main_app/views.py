169a6d05ef58a884bcf349f4e03d7512ec5c4807
Kiran Roge <kiranroge19@gmail.com>
2025-04-28 10:46:07 -0500

    wip:added home page, all plants page and about page with styles

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 def home(request):
     #each view function takes a request object as an argument
     #request object contains metadata about the request
-    return HttpResponse('<h1>hello world</h1>')
+    return render(request, 'home.html')
     #HttpResponse is a class that takes a string as an argument
     #this is the response that will be sent to the client by returning it
 
@@ -17,4 +17,25 @@ def about(request):
     return render(request, 'about.html', {
         'contact_info': contact_info
     })
+
+# views.py
+
+class Plant:
+    def __init__(self, name, species, description, age):
+        self.name = name
+        self.species = species
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+plants = [
+    Plant('Money Plant', 'Pilea peperomioides', 'Money Plant is a symbol of prosperity and good fortune.', 1),
+    Plant('Snake Plant', 'Sansevieria trifasciata', 'Snake Plant is a symbol of prosperity and good fortune.', 2),
+    Plant('Dumb Cane', 'Dracaena sp.', 'Dumb Cane is a symbol of prosperity and good fortune.', 3),
+    Plant('Pothos', 'Epipremnum aureum', 'Pothos is a symbol of prosperity and good fortune.', 1)
+]
+
+
+def plants_index(request):
+    return render(request, 'plants/index.html', { 'plants' : plants })
 
